02307083eb4f7070dc6f8f554fa7f63bb352de7f
moadams <adamsmichael46@gmail.com>
2024-01-31 18:07:01 +0000

    Navbar and hero section complete

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import './Hero.css'
+import Me from "../../assets/images/me.jpg"
+import ReactIcon from "../../assets/images/react.png"
+import Django from "../../assets/images/django.png"
+
+const Hero = () => {
+  return (
+    <section className="hero-container">
+        <div className="hero-content">
+            <h2>Building Digital Experiences That Inspire</h2>
+            <p>
+                Passionate Full Stack Developer | Transforming ideas into Seamless and Visually Stunning Web Solutions
+            </p>
+        </div>
+
+        <div className="hero-img">
+            <div>
+                <div className="tech-icon">
+                    <img src={ReactIcon} alt="react" />
+                </div>
+                <img src={Me} alt="me" />
+            </div>
+            <div>
+                <div className="tech-icon">
+                    <img src={Django} alt="react" />
+                </div>
+                <div className="tech-icon">
+                    <img src={ReactIcon} alt="react" />
+                </div>
+                <div className="tech-icon">
+                    <img src={ReactIcon} alt="react" />
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
